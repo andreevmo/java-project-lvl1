b1ddf3e417d15c027a138ce22463990c4656b330
Andreev Maksim <maxim_525@mail.ru>
2022-07-12 22:02:07 +0400

    refactor: update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Cli {
 
+    private static String userName = "user";
+
+    public static String getUserName() {
+        return userName;
+    }
+
     public static void makeWelcome() {
 
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.nextLine();
+        userName = scanner.nextLine();
 
         System.out.println("Hello, " + userName + "!");
     }
