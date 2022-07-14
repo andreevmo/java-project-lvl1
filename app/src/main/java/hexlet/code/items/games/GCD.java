8b321376808f3e204db32d00009da9ca24787f3e
Andreev Maksim <maxim_525@mail.ru>
2022-07-14 13:41:21 +0400

    feat: add game GCD
    GCD - ask the user to find the greatest common divisor

---
+++ app/src/main/java/hexlet/code/items/games/GCD.java
@@ -0,0 +1,71 @@
+package hexlet.code.items.games;
+
+import hexlet.code.Cli;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.Scanner;
+
+public class GCD implements Game {
+
+    private String nameItem = "GCD";
+    private int numberItem = 4;
+    private int[] operands;
+    private String userAnswer;
+    private int result;
+    private Scanner scanner = new Scanner(System.in);
+    @Override
+    public final int getNumItem() {
+        return this.numberItem;
+    }
+
+    @Override
+    public final String getNameItem() {
+        return this.nameItem;
+    }
+
+    @Override
+    public final void greet() {
+        Cli.makeWelcome();
+        System.out.println("Find the greatest common divisor of given numbers.");
+    }
+
+    @Override
+    public final void generate() {
+        operands = new int[]{RandomUtils.nextInt(1, 100), RandomUtils.nextInt(1, 100)};
+
+        result = getGCD(operands[0], operands[1]);
+    }
+
+    @Override
+    public final void ask() {
+        System.out.println("Question: " + operands[0] + " " + operands[1]);
+    }
+
+    @Override
+    public final void getAnswer() {
+        System.out.print("Your answer: ");
+        userAnswer = scanner.nextLine();
+    }
+
+    @Override
+    public final boolean checkAnswer() {
+        boolean isCorrect = false;
+
+        boolean correctUserInput = new Scanner(userAnswer).hasNextInt();
+
+
+        if (correctUserInput && result == Integer.parseInt(userAnswer)) {
+            System.out.println("Correct!");
+            isCorrect = true;
+        } else {
+            System.out.println("'" + userAnswer + "' is wrong answer ;(. "
+                    + "Correct answer was '" + result + "'.\n"
+                    + "Let's try again, " + Cli.getUserName() + "!");
+        }
+        return isCorrect;
+    }
+
+    private int getGCD(int a, int b) {
+        return Math.max(a, b) % Math.min(a, b) == 0 ? b : getGCD(b, a % b);
+    }
+}
