3e77a4c57050337803a598ec9b2b608f0cd54f4d
Andreev Maksim <maxim_525@mail.ru>
2022-07-15 18:23:00 +0400

    refactor: update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,14 +7,23 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+
+        Game[] items = {new Even(), new Calc(), new GCD(), new Progression(), new Prime()};
+        showMenu(items);
+        openGame(items);
+
+    }
+
+    public static void showMenu(Game[] items) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
-        Game[] items = {new Even(), new Calc(), new GCD(), new Progression(), new Prime()};
         for (Game game : items) {
             System.out.println(game.getNumItem() + " - " + game.getNameItem());
         }
         System.out.println("0 - Exit");
+    }
 
+    public static void openGame(Game[] items) {
         System.out.print("Your choice: ");
         String userChoice = new Scanner(System.in).nextLine();
         userChoice = new Scanner(userChoice).hasNextInt() ? userChoice : "0";
